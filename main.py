a894d35dcadcf6a0d276157b8e0f3f90db585232
Ashu8083n <asutosh.gouda22@gmail.com>
2026-07-26 13:57:45 +0530

    router connect to main

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from fastapi import FastAPI
 from fastapi.routing import APIRouter
 
+from app.api.user_api import user_router
+
 
 app = FastAPI()
 
-router = APIRouter()
+app.include_router(user_router)  # Include your user_router here
 
 @app.get("/")
 async def hello():
